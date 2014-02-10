bed1947a4f0c743f12df317a0a3d71854a0557db
Giovanni Paolini <giove.paolini@gmail.com>
2014-02-10 23:01:29 +0100

    Aggiunto constants.py

---
+++ game/constants.py
@@ -0,0 +1,29 @@
+# Turn phases
+DAY = 'D'
+SUNSET = 'S'
+NIGHT = 'N'
+DAWN = 'W'
+
+# Teams
+POPOLANI = 'P'
+LUPI = 'L'
+NEGROMANTI = 'N'
+
+# Auras
+WHITE = 'W'
+BLACK = 'B'
+
+# Supernatural powers
+AMNESIA = 'A'
+DUPLICAZIONE = 'D'
+ILLUSIONE = 'I'
+MISTIFICAZIONE = 'M'
+MORTE = 'R'
+OCCULTAMENTO = 'O'
+OMBRA = 'B'
+VISIONE = 'V'
+
+# Commands
+USEPOWER = 'P'
+VOTE = 'V'
+ELECT = 'E'
