b34de9b1cc63bd85da40bd3f8718bf413ea0379f
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2014-03-17 00:56:39 +0100

    Script to perform turn advance.

---
+++ perform_turn_advance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import json
+import datetime
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lupus.settings")
+from game.models import *
+from game.tests import *
+from game.utils import *
+from game.constants import *
+
+def main():
+    game = Game.get_running_game()
+    game.advance_turn()
+
+if __name__ == '__main__':
+    main()
