c635127d52b2fad335787937095cd7d63d054283
Giovanni Paolini <giovanni.paolini@sns.it>
2014-03-17 01:17:23 +0100

    Cambiato il redirect.

--- lupus/urls.py
+++ lupus/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 from game import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='lupus/game/', permanent=False)),
+    url(r'^$', RedirectView.as_view(url='/lupus/game/index/', permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
