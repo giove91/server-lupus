e350080a801fe7dedc85dd3255fc9094fa2b9dd2
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2014-11-16 15:47:09 +0100

    Test composition.

--- game/scratch.py
+++ game/scratch.py
@@ -1,33 +1,38 @@
 # -*- coding: utf-8 -*-
 
+# Composition for a test village
+
 from roles import *
 
 test_roles = [
     Contadino, Contadino, Contadino,
-    Cacciatore,
-    Custode,
-    Divinatore,
-    Esorcista,
-    Espansivo,
-    Guardia,
-    Investigatore,
-    Mago,
-    Massone,
-    Messia,
-    Trasformista,
-    Stalker,
-    Veggente,
-    Voyeur,
+    Cacciatore, Cacciatore,
+    Custode, Custode,
+    Divinatore, Divinatore,
+    Esorcista, Esorcista,
+    Espansivo, Espansivo,
+    Guardia, Guardia,
+    Investigatore, Investigatore,
+    Mago, Mago,
+    Massone, Massone,
+    Messia, Messia,
+    Sciamano, Sciamano,
+    Stalker, Stalker,
+    Trasformista, Trasformista,
+    Veggente, Veggente,
+    Voyeur, Voyeur,
+
     Lupo, Lupo, Lupo,
-    Avvocato,
-    Diavolo,
-    Fattucchiera,
-    Sciamano,
-    Rinnegato,
+    Avvocato, Avvocato,
+    Diavolo, Diavolo,
+    Fattucchiera, Fattucchiera,
+    Rinnegato, Rinnegato,
     Sequestratore, Sequestratore,
+
     Negromante, Negromante, Negromante,
-    Fantasma,
+    Fantasma, Fantasma,
     Ipnotista, Ipnotista,
-    Medium,
+    Medium, Medium,
+    Scrutatore, Scrutatore,
     ]
 
