83c7ba2233c025fc3866ecfa5a1233f942e55f57
Giovanni Paolini <giove.paolini@gmail.com>
2014-11-20 00:36:46 +0100

    Patch in AppointView.

--- game/views.py
+++ game/views.py
@@ -577,6 +577,9 @@ class AppointView(CommandView):
         game = player.game
         target = cleaned_data['target']
         
+        if target == '':
+            target = None
+        
         if target is not None and target not in game.get_alive_players():
             return False
         
