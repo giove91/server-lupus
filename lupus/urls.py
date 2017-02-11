87730695601b2f8dfb9ac41a4ed66453e21165c2
Matteo Migliorini <matteo.migliorini@sns.it>
2017-02-11 11:54:46 +0100

    Implementato la funzione di cambio password.
    Ho creato i templates con il form di cambio password che usano le
    funzioni predefinite di Django, e ho aggiunto il link in alto a
    sinistra sotto al nome utente. Non è che mi piaccia molto in
    quella posizione, ma se avete idee migliori... (forse in un menu`
    a sinistra?

--- lupus/urls.py
+++ lupus/urls.py
@@ -12,6 +12,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+	url(r'^password_change/$', 'django.contrib.auth.views.password_change', 
+		{'template_name':'registration/password_change_form.html'}, name='password_change'),
+	url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', 
+		{'template_name':'registration/password_change_done.html'}, name='password_change_done'),
     url(r'^game/', include('game.urls')),
 )
 
