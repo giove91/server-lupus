d7c00dffa43883bb3d4d17e80b13d7c6ac85dd80
Matteo Migliorini <miglio1995@gmail.com>
2018-08-11 15:09:57 +0200

    Aggiornati test avanzamento tempo.

--- game/tests/tests.py
+++ game/tests/tests.py
@@ -51,53 +51,47 @@ class AdvanceTimeTests(TestCase):
     def test_simple_advance_with_useless_skip(self):
         now = datetime(2015, 11, 11, 16, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 11, 11, 18, 0, 0), is_dst=None))
 
     def test_wrapped_advance_with_useless_skip(self):
         now = datetime(2015, 11, 11, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 11, 12, 18, 0, 0), is_dst=None))
 
     def test_enter_dst_advance_with_useless_skip(self):
         now = datetime(2015, 3, 28, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 3, 29, 18, 0, 0), is_dst=None))
 
     def test_exit_dst_advance_with_useless_skip(self):
         now = datetime(2015, 10, 24, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, )
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 10, 25, 18, 0, 0), is_dst=None))
 
     def test_simple_advance_with_real_skip(self):
         now = datetime(2015, 11, 13, 16, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 11, 15, 18, 0, 0), is_dst=None))
 
     def test_wrapped_advance_with_real_skip(self):
         now = datetime(2015, 11, 12, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 11, 15, 18, 0, 0), is_dst=None))
 
     def test_enter_dst_advance_with_real_skip(self):
         now = datetime(2015, 3, 26, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 3, 29, 18, 0, 0), is_dst=None))
 
     def test_exit_dst_advance_with_real_skip(self):
         now = datetime(2015, 10, 22, 20, 0, 0, tzinfo=REF_TZINFO)
         when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
+        later = advance_to_time(now, when, allowed_weekdays=[0, 1, 2, 3, 6])
         self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 10, 25, 18, 0, 0), is_dst=None))
-
-    def test_wrapped_advance_through_immacolate_bridge(self):
-        now = datetime(2015, 12, 3, 20, 0, 0, tzinfo=REF_TZINFO)
-        when = time(18, 0, 0)
-        later = advance_to_time(now, when, day_end_skip=True)
-        self.assertEqual(later, REF_TZINFO.localize(datetime(2015, 12, 8, 18, 0, 0), is_dst=None))
